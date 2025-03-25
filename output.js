47c1f800574fd7652efa30b066655b90218dab82
Najay22 <nramirez4@albany.edu>
2025-03-24 20:06:05 -0400

    Create output.js
    output java

---
+++ output.js
@@ -0,0 +1,13 @@
+function showInnerHTML() {
+	document.getElementById("output1").innerHTML = "Hello from innerHTML!";
+}
+function useDocumentWrite() {
+	document.write("This replaces the page content! not recommended.");
+}
+function showAlert() {
+alert("WahhGwannn!");
+}
+function showConsole() {
+ console.log("this message is in the console.");
+ }
+	
